d88a77a659264501162c8fbe97d82d41d5cf4e4b
Michal Wadas <michalwadas@onet.pl>
2022-04-28 00:02:26 +0200

    Preparing divs in main Game file.

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import Board from "./Board";
+
 
 
 export default function Game() {
+    const moveInformation = "Next move will be player O"
+    const listOfMoves = (
+        <li>
+            <button> Start</button>
+        </li>
+    )
     return (
         <div>
+            <div className="board-game">
+                <Board> </Board>
+            </div>
+
+                <div className="info-game">
+                    <div>{moveInformation}</div>
+                    <ul>{listOfMoves}</ul>
+                </div>
         </div>
     )
 }
