eedfa38ca4fa09dc83a5bb95a4cc7ee062c595f9
Michal Wadas <michalwa@backbase.com>
2022-05-14 02:19:09 +0200

    Adding firebase settings.

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,10 +1,9 @@
 import React, {useReducer} from 'react';
 import Board from "./Board";
 import {Redirect, useHistory} from "react-router-dom";
-import win from '../resources/win.wav'
 import click from '../resources/click.wav'
-import draw from '../resources/draw.wav'
 import back from '../resources/backStep.wav'
+import {calculateWinner} from './calculateWinner'
 
 export default function Game({authorized}) {
 
@@ -131,41 +130,41 @@ export default function Game({authorized}) {
     )
 }
 
-const calculateWinner = (squares) => {
-    const drawP = new Audio(draw)
-    const winP = new Audio(win)
-
-    const clickDraw = () => {
-        drawP.play()
-    }
-    const clickWin = () => {
-        winP.play()
-    }
-
-    const winLines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-    ];
-    let isDraw = true;
-    for (let i = 0; i < winLines.length; i++) {
-        const [a, b, c] = winLines[i];
-        if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c]) {
-            clickWin();
-            return squares[a];
-        }
-        if (!squares[a] || !squares[b] || !squares[c]) {
-            isDraw = false;
-        }
-    }
-    if (isDraw) {
-        clickDraw();
-        return 'Draw';
-    }
-    return null;
-}
+// const calculateWinner = (squares) => {
+//     const drawP = new Audio(draw)
+//     const winP = new Audio(win)
+//
+//     const clickDraw = () => {
+//         drawP.play()
+//     }
+//     const clickWin = () => {
+//         winP.play()
+//     }
+//
+//     const winLines = [
+//         [0, 1, 2],
+//         [3, 4, 5],
+//         [6, 7, 8],
+//         [0, 3, 6],
+//         [1, 4, 7],
+//         [2, 5, 8],
+//         [0, 4, 8],
+//         [2, 4, 6]
+//     ];
+//     let isDraw = true;
+//     for (let i = 0; i < winLines.length; i++) {
+//         const [a, b, c] = winLines[i];
+//         if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c]) {
+//             clickWin();
+//             return squares[a];
+//         }
+//         if (!squares[a] || !squares[b] || !squares[c]) {
+//             isDraw = false;
+//         }
+//     }
+//     if (isDraw) {
+//         clickDraw();
+//         return 'Draw';
+//     }
+//     return null;
+// }
